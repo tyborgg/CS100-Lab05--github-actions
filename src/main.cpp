73115e4d1916853c9ecb16ae5eff76ba0aad5633
tyborgg <tpast001@ucr.edu>
2020-11-08 14:53:04 -0800

    Added the rectangle.cpp for the passing tests and implemented main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "rectangle.hpp"
+
+using namespace std;
+
+int main(){
+
+int width = 0;
+int height = 0;
+Rectangle call;
+
+cout << "Perimeter: " << call.perimeter(4,2) << endl;
+cout << "Area: " << call.area(4,2) << endl;
+
+cin >> width;
+cin >> height;
+
+cout << "Perimeter: " << call.perimeter(width,height) << endl;
+cout << "Area: " << call.area(width,height) << endl;
+
+
+return 0;
+}
