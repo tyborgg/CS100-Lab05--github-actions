950c426ec7287bdd160105ba51541a9297796abc
tyborgg <tpast001@ucr.edu>
2020-11-08 15:51:38 -0800

    Added proper return statements for negative or zero inputs

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -18,10 +18,18 @@ void Rectangle::set_height(int h){
 }
 
 int Rectangle::area(){
+	if(width <= 0 || height <= 0){
+		return "Error: cannot accept negative or zero values";
+	}
+
 	return width * height;
 }
 
 int Rectangle::perimeter(){
+	if(width <= 0 || height <= 0){
+                return "Error: cannot accept negative or zero values";
+        }
+
 	int sideW = width * 2;
 	int sideH = height * 2;
 	int add = sideH + side W;
